e3212559def2ca6285949e716f4770926641de16
bdvsnaidu <127775632+bdvsnaidu@users.noreply.github.com>
2023-08-05 16:46:03 +0530

    Update and rename approvedDeposit.js to depositTransfer.js

--- scripts/depositTransfer.js
+++ scripts/depositTransfer.js
@@ -1,27 +1,32 @@
+//import c from "contracts/examples/erc721-transfer/FxERC721RootTunnel.sol"// Import necessary packages and contracts
 const { ethers } = require("hardhat");
 const { FXRootContractAbi } = require("../artifacts/FXRootContractAbi.js");
 const ABI = require("../artifacts/contracts/police_cop.sol/police_cop.json");
 require("dotenv").config();
 
+//Transfer ERC721A tokens to the Ethereum FxChain network
 async function main() {
+  // Set up connections to network and wallet
   const networkAddress =
     "https://eth-goerli.g.alchemy.com/v2/h-asDNMJ21mVniDAY3XE1VQ9F7PW7A0x";
   const privateKey = process.env.PRIVATE_KEY;
   const provider = new ethers.providers.JsonRpcProvider(networkAddress);
 
+  // Create a wallet instance
   const wallet = new ethers.Wallet(privateKey, provider);
 
+  // Get the signer instance
   const [signer] = await ethers.getSigners();
 
   // Get ERC721A contract instance
   const NFT = await ethers.getContractFactory("police_cop");
-  const nft = await NFT.attach("0x718730E802DeFf7A00784a5C20920BDfF9945583");
+  const nft = await NFT.attach("0x08b7f3BFB7e7acA78156d53d7499aAA6c52E1FF6");
 
   // Get FXRoot contract instance
   const fxRootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
   const fxRoot = await ethers.getContractAt(FXRootContractAbi, fxRootAddress);
 
-  // TokenIds to transfer (police_cop)
+  // TokenIds to transfer
   const tokenIds = [0, 1, 2, 3, 4];
 
   // Approve the nfts for transfer
@@ -31,22 +36,28 @@ async function main() {
   await approveTx.wait();
   console.log("Approval confirmed");
 
-  const receiverMumbaiAddress = '0x2513419a865De1a265d37935b25A65D54A66D143';
-
   // Deposit the nfts to the FXRoot contracts
   for (let i = 0; i < tokenIds; i++) {
     const depositTx = await fxRoot
       .connect(signer)
-      .deposit(nft.address, receiverMumbaiAddress, tokenIds[i], "0x6566");
+      .deposit(nft.address, wallet.address, tokenIds[i], "0x6566");
 
     // Wait for the deposit to be confirmed
     await depositTx.wait();
   }
 
-  console.log("Approved and deposited to polygon mumbai network");
+  console.log("Approved and deposited");
 
+  // Test balanceOf
+  const balance = await nft.balanceOf(wallet.address);
 
-  
+  // Print the balance of the wallet
+  console.log(
+    "IndianNFT wallet balance",
+    wallet.address,
+    "is: ",
+    balance.toString()
+  );
 }
 
 // Call the main function and handle any errors
